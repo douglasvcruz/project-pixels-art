d9c602c0ad603275c95ca58fbce5b0b4a87cdc0f
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-09 20:49:30 -0300

    6º, 7º, 8º, 9º, 10º, 11º Requisito

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const color = document.querySelectorAll('.color');
+const bottom = document.querySelector('#button-random-color');
+const board = document.querySelector('#pixel-board');
+const reseta = document.querySelector('#clear-board');
+
 function rgb() {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
@@ -6,9 +11,6 @@ function rgb() {
 }
 
 function corAleatoria() {
-  const bottom = document.querySelector('#button-random-color');
-  const color = document.getElementsByClassName('color');
-
   bottom.addEventListener('click', () => {
     for (let i = 1; i < color.length; i += 1) {
       color[i].style.backgroundColor = rgb();
@@ -17,3 +19,56 @@ function corAleatoria() {
 }
 
 corAleatoria();
+
+function pixeis() {
+  for (let i = 0; i < 25; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    board.appendChild(pixel);
+  }
+}
+
+pixeis();
+
+const pixels = document.querySelectorAll('.pixel');
+
+function remover() {
+  for (let i = 0; i < color.length; i += 1) {
+    color[i].className = 'color';
+  }
+}
+
+function selecionar() {
+  for (let i = 0; i < color.length; i += 1) {
+    color[i].addEventListener('click', () => {
+      remover();
+      color[i].className = 'color selected';
+    });
+  }
+}
+
+selecionar();
+
+function colorir() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      const selected = document.querySelector('.selected');
+      if (pixels[i].style.backgroundColor !== selected.style.backgroundColor) {
+        pixels[i].style.backgroundColor = selected.style.backgroundColor;
+      } else {
+        pixels[i].style.backgroundColor = 'white';
+      }
+    });
+  }
+}
+colorir();
+
+function reset() {
+  reseta.addEventListener('click', () => {
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].style.backgroundColor = 'white';
+    }
+  });
+}
+
+reset();
